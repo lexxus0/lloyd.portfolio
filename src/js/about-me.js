b892eba2ac7c36e625786be621d417e97d795c8c
benchik <olgalitvinova1905@icloud.com>
2024-07-05 15:37:58 +0200

    updt about-me.html and about-me.css; add about-me.js; updt icons.svg,

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,5 +1,26 @@
 // import Swiper from 'swiper';
 // import 'swiper/css';
 
-// import Accordion from 'accordion-js';
+import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container', {
+  duration: 600,
+  showMultiple: true,
+  openOnInit: [0],
+});
+
+const accordionsList = document.querySelectorAll('.about-accordion-item');
+accordionsList.forEach(function (item) {
+  const title = item.querySelector('.about-accordion-title');
+  const btnDown = item.querySelector('.ac-trigger');
+  btnDown.addEventListener('mouseenter', function () {
+    title.classList.add('hovered');
+  });
+
+  btnDown.addEventListener('mouseleave', function () {
+    title.classList.remove('hovered');
+  });
+});
+
+

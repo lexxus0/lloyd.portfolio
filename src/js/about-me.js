310fe167ee3633a88a8d65075b05e38773f8cbc2
benchik <olgalitvinova1905@icloud.com>
2024-07-05 21:05:31 +0200

    updt about-me.html, about-me.css, about-me.js, icons.svg

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,5 +1,5 @@
-// import Swiper from 'swiper';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import 'swiper/css';
 
 import Accordion from 'accordion-js';
 // import 'accordion-js/dist/accordion.min.css';
@@ -20,6 +20,54 @@ accordionsList.forEach(function (item) {
 
   btnDown.addEventListener('mouseleave', function () {
     title.classList.remove('hovered');
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.about-skills-item');
+  const swiperWrapper = document.querySelector('.swiper-wrapper');
+  slides.forEach((slide, index) => {
+    const clone = slide.cloneNode(true);
+    swiperWrapper.appendChild(clone);
+  });
+
+  const swiper = new Swiper('.about-skills-swiper', {
+    speed: 400,
+    spaceBetween: 0,
+    loop: true,
+    setWrapperSize: true,
+    breakpoints: {
+      // Налаштування для десктопу
+      1440: {
+        slidesPerView: 6,
+      },
+      // Налаштування для планшету
+      768: {
+        slidesPerView: 3,
+      },
+      // Налаштування для мобільних пристроїв
+      320: {
+        slidesPerView: 2,
+      },
+    },
+    on: {
+      init: function () {
+        document.querySelector('.swiper-slide').style.backgroundColor =
+          '#ed3b44';
+      },
+      slideChange: function () {
+        const previousSlide = this.slides[this.previousIndex];
+        const activeSlide = this.slides[this.activeIndex];
+        previousSlide.style.background = '';
+        activeSlide.style.backgroundColor = '#ed3b44';
+      },
+    },
+  });
+
+  const btnNext = document.querySelector('.about-skills-btn');
+  btnNext.addEventListener('click', () => {
+    swiper.slideNext();
   });
 });
 
